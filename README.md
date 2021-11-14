505d8047276d7409a4b53cf7cf52ad5a1cbde97b
karenlimacdo <karenlmacedo16@gmail.com>
2021-11-14 20:56:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mfe
+
+Material criado seguindo o curso "MicroFrontends with React: A Complete Developer's Guide" em https://ibm-learning.udemy.com/course/microfrontend-course/learn/lecture/23207034
---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,17 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/, //quando importarmos um arquivo que termine com mjs ou apenas js, o babel deve processar
+                exclude: /node_modules/, //babel não deve processar arquivos de node_modules
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime'],
+                    },
+                },
+            },
+        ],
+    },
+};
